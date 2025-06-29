e66fc76831438b63653ce74cfb30b506bd809377
tyronmaster <degonski.art@gmail.com>
2025-06-29 13:09:25 +0200

    init: start Atipera project

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'Atipera';
